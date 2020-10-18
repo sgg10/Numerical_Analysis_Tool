ff05c80b38389428132ccdf37ab55780ba6368e5
Rafael <fsociety.fenix@gmail.com>
2020-10-17 19:44:10 -0500

    fixed __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
 from flask import Flask
-from .Converions import conversions
+from .Conversions import conversions
 from .Errors import errors
-from .Rounded import rounded
-from .Biseccion import biseccion
-from .ReglaFalsa import reglaFalsa
+from .Rounding import rounding
+from .Bisection import bisection
+from .False_Position import false_position
 from .Newton import newton
-from .Secante import secante 
-from .PuntoFijo import puntoFijo
-from .RaicesMultiples import raicesMultiples
-from .BusquedaIncrementales import busquedaIncrementales
+from .Secant import secant
+from .Fixed_Point_Iteration import fixed_point_iteration
+from .Multi_Roots import multi_roots
+from .Incremental_Search import incremental_search
 
 def create_app():
   app = Flask(__name__)
   app.register_blueprint(conversions)
   app.register_blueprint(errors)
-  app.register_blueprint(rounded)
-  app.register_blueprint(biseccion)
-  app.register_blueprint(reglaFalsa)
+  app.register_blueprint(rounding)
+  app.register_blueprint(bisection)
+  app.register_blueprint(false_position)
   app.register_blueprint(newton)
-  app.register_blueprint(secante)
-  app.register_blueprint(puntoFijo)
-  app.register_blueprint(raicesMultiples)
-  app.register_blueprint(busquedaIncrementales)
+  app.register_blueprint(secant)
+  app.register_blueprint(fixed_point_iteration)
+  app.register_blueprint(multi_roots)
+  app.register_blueprint(incremental_search)
   return app
