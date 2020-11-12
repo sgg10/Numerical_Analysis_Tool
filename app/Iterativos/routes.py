d3487051be81da7e6b3181b9b03fcce9c8052538
Sebastian Granda Gallego <sgrandag@eafit.edu.co>
2020-11-11 21:53:07 -0500

    Add Some methods

--- app/Iterativos/routes.py
+++ app/Iterativos/routes.py
@@ -1,5 +1,5 @@
 from . import iterativos
-from ..Methods.Iterativos import GaussSeidel, Jacobi, SOR
+from ..Methods.Iterativos import GaussSeidel, Jacobi, SOR, Vandermonde
 
 @iterativos.route('/gaussseidel/<n>&<A>&<b>&<x0>&<niter>&<tol>')
 def metodoGaussSeidel(n, A, b, x0, niter, tol):
@@ -12,3 +12,11 @@ def metodoJacobi(n, A, b, x0, niter, tol):
 @iterativos.route('/sor/<n>&<A>&<b>&<x0>&<omega>&<niter>&<tol>')
 def metodoSOR(n, A, b, x0, omega, niter, tol):
   return SOR.SOR(n, A, b, x0, omega, niter, tol).run()
+
+@iterativos.route('/vandermonde/<x>')
+def metodoVandermonde(x):
+  x = x.replace('[', '').replace(']', '').split(',')
+  print(x)
+  for i in range(len(x)):
+    x[i] = float(x[i])
+  return Vandermonde.Vandermonde(x).run()
